83a82331a93238c82e9f617e38cc87f9dd01d9f4
Jeganathan <jkarthike@gmail.com>
2021-05-12 20:52:29 +0200

    Added routers

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Routers
+const bootcamps = require('./routes/bootcamps');
+
 // Load Env vars
 dotenv.config({
     path: './config/config.env',
 });
 
 const app = express();
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
