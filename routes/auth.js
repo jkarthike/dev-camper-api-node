c768b6bc5218c304cd7ebd5174cb449c9cdd88e6
Jeganathan <jkarthike@gmail.com>
2021-05-23 11:06:32 +0200

    User and auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe } = require('../controllers/auth');
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword,
+    resetPassword,
+    updateUserDetails,
+    updatePassword,
+} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updateUserDetails', protect, updateUserDetails);
+router.put('/updatePassword', protect, updatePassword);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetPassword/:resettoken', resetPassword);
 
 module.exports = router;
